b601590ea7de467efadbaf5643d5f29b1ca70834
Canul <canuladriana24@gmail.com>
2024-04-06 20:38:53 -0600

    Logica del login

--- app/src/main/java/com/example/pizzeriaapp/MainActivity.java
+++ app/src/main/java/com/example/pizzeriaapp/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.pizzeriaapp;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +16,40 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText usuario, password;
+    Button iniciar;
+    SharedPreferences sharedPreferences;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        iniciar = findViewById(R.id.iniciar);
+        usuario = findViewById(R.id.usuario);
+        password = findViewById(R.id.password);
+
+        sharedPreferences = getSharedPreferences("Mis preferencias", Context.MODE_PRIVATE);
+
+        iniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = usuario.getText().toString();
+                String pass = password.getText().toString();
+
+                usuario.setText("");
+                password.setText("");
+
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("usuario", user);
+                editor.putString("password", pass);
+                editor.apply();
+
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
         });
     }
 }
