e41d546e8221c231be1f33a86e916c851ef88fb5
Canul <canuladriana24@gmail.com>
2024-04-07 03:16:19 -0600

    cambios en la logica de la segunda ventana

--- app/src/main/java/com/example/pizzeriaapp/MainActivity2.java
+++ app/src/main/java/com/example/pizzeriaapp/MainActivity2.java
@@ -1,6 +1,9 @@
 package com.example.pizzeriaapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,33 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity2 extends AppCompatActivity {
 
-    @Override
+    Button btnPizzas;
+    Button btnBebidas;
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main2);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnPizzas = findViewById(R.id.pizzas);
+        btnBebidas = findViewById(R.id.bebidas);
+
+        btnPizzas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity2.this, MainActivity3.class);
+                startActivity(intent);
+            }
+        });
+
+        btnBebidas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity2.this, MainActivity4.class);
+                startActivity(intent);
+            }
         });
     }
 }
